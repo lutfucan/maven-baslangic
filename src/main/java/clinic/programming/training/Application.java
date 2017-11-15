6013289ac854a878fdd14ea72f8b30e956befde2
lutfucan <lutfucan@hotmail.com>
2017-11-15 16:32:31 +0300

    StrinUtils'ten lang3 dependency eklendi

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,8 +2,14 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+	
+	public int countWords(String words) {
+		String[] seperateWords = StringUtils.split(words, ' ');
+		return (seperateWords == null) ? 0 : seperateWords.length;
+	}
 	
 	public void greet() {
 		List<String> greetings = new ArrayList<>();
@@ -22,6 +28,8 @@ public class Application {
 		System.out.println("Starting Application");
 		Application app = new Application();
 		app.greet();
+		int count = app.countWords("Dört kelimesi olan cümlem");
+		System.out.println("Kelime sayısı : " + count);
 	}
 	
 }
